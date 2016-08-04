c2e2aff3f481a8b44bf9c8b491b74cc95f0b1161
Fabian Fink <fabian.fink@db.com>
2016-08-04 12:06:43 +0200

    added missing files

---
+++ app/models/festival-model.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// This is the generic schema for an article
+module.exports = mongoose.model('Festival', new Schema({
+  festivalName: String,
+  name: String,
+  surname: String,
+  facebookUrl: String,
+  homepageUrl: String,
+  email: String,
+  address: {
+    route: String,
+    street_number: String,
+    locality: String,
+    postal_code: String,
+    country: String,
+    distance: String,
+    lat: Number,
+    lng: Number
+  }
+}));
